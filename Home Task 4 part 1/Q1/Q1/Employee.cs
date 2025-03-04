96dd8efca5da6dbb4d03a89de4e78877385eb28e
Assamul Haq <assamulhaq.ntu.1184@gmail.com>
2025-03-04 20:29:24 +0500

    add comment

--- Home Task 4 part 1/Q1/Q1/Employee.cs
+++ Home Task 4 part 1/Q1/Q1/Employee.cs
@@ -8,6 +8,7 @@ namespace Q1
 {
     class Employee
     {
+        //this is a employee class
        
         public int ID;
         public string Name;
@@ -21,7 +22,6 @@ namespace Q1
             Department = department;
             Salary = salary;
         }
-
         public void Display()
         {
 
